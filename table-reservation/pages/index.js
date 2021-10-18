4b218e40779e05780789600770bdab6d6dfb694c
aaryan <aaryanpatil812@yahoo.com>
2021-10-18 18:16:51 +0530

    RestList added

--- table-reservation/pages/index.js
+++ table-reservation/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className={styles.PageLayout}>
       <Navbar />
-      <RestSignup />
+      {/* <RestSignup /> */}
       <RestList />
       <Footer />
 
